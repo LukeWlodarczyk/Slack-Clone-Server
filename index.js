148514c101628d2dacc233112c3233ce90c2eb15
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-30 21:48:46 +0200

    merge resolvers and schemas

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 import express from 'express';
 import { ApolloServer, gql, graphiqlExpress } from 'apollo-server-express';
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
-import typeDefs from './schema';
-import resolvers from './resolvers';
 import models from './models';
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
+
+const resolvers = mergeResolvers(
+	fileLoader(path.join(__dirname, './resolvers'))
+);
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: { models, user: { id: 1 } },
+});
 
 const app = express();
 server.applyMiddleware({ app });
 
 const port = process.env.PORT || 5000;
 
-models.sequelize.sync({ force: true }).then(() => {
+models.sequelize.sync().then(() => {
 	app.listen(port, () => {
 		console.log(
 			`Server is ready at http://localhost:5000${server.graphqlPath}`
