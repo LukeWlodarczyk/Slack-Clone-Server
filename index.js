3a0fba9fc97abf50576167d3483f1fe29428bfbf
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-30 17:29:39 +0200

    express graphql setup

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { ApolloServer, gql, graphiqlExpress } from 'apollo-server-express';
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+	console.log(`Server is ready at http://localhost:5000${server.graphqlPath}`);
+});
