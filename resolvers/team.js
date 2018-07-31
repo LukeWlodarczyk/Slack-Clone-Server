82205aa9b728337141934ee92635d97401211772
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-31 17:08:55 +0200

    permissions, update resolvers and team model validation

--- resolvers/team.js
+++ resolvers/team.js
@@ -1,19 +1,22 @@
 import { formatErrors } from '../helpers/formatErrors';
+import { requiresAuth } from '../helpers/permissions';
 
 export default {
 	Mutation: {
-		createTeam: async (parent, args, { models, user }) => {
-			try {
-				await models.Team.create({ ...args, owner: user.id });
-				return {
-					success: true,
-				};
-			} catch (err) {
-				return {
-					success: false,
-					errors: formatErrors(err),
-				};
+		createTeam: requiresAuth.createResolver(
+			async (parent, args, { models, user }) => {
+				try {
+					await models.Team.create({ ...args, owner: user.id });
+					return {
+						success: true,
+					};
+				} catch (err) {
+					return {
+						success: false,
+						errors: formatErrors(err, models),
+					};
+				}
 			}
-		},
+		),
 	},
 };
