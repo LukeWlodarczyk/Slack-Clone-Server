e8a4e8eb01c9d411323fcc37b5ca341288f26fa2
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-08-12 19:22:29 +0200

    RedisPubSub

--- helpers/pubsub.js
+++ helpers/pubsub.js
@@ -1,5 +1,9 @@
-import { PubSub } from 'apollo-server-express';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
 
-const pubsub = new PubSub();
-
-export default pubsub;
+export default new RedisPubSub({
+	connection: {
+		host: process.env.REDIS_HOST || '127.0.0.1',
+		port: 6379,
+		retry_strategy: options => Math.max(options.attempt * 100, 3000),
+	},
+});
