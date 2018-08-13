889bf2a16a9d73c0f9d1339bf2ea68e605e1a88c
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-08-13 07:12:54 +0200

    docker-compose

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,7 @@ import Sequelize from 'sequelize';
 const sequelize = new Sequelize('slack', 'postgres', 'hellopostgres', {
 	dialect: 'postgres',
 	operatorsAliases: Sequelize.Op,
+	host: process.env.DB_HOST || 'localhost',
 	define: {
 		underscored: true,
 	},
